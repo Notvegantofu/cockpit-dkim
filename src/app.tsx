e95cbc5e3a3dc7c6ce0e242c0c6ad0c86f9bbe62
Tobias Vale <tobias.vale@protonmail.com>
2025-03-28 12:09:39 +0100

    Added copyright notice to the start of files

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
 /*
- * This file is part of Cockpit.
+ * This file is part of Cockpit-dkim.
  *
- * Copyright (C) 2017 Red Hat, Inc.
+ * Copyright (C) 2025 Tobias Vale
  *
- * Cockpit is free software; you can redistribute it and/or modify it
+ * Cockpit-dkim is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * Cockpit is distributed in the hope that it will be useful, but
+ * Cockpit-dkim is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
--- src/components/HorizontalNav.tsx
+++ src/components/HorizontalNav.tsx
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Cockpit-dkim.
+ *
+ * Copyright (C) 2025 Tobias Vale
+ *
+ * Cockpit-dkim is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit-dkim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import { Nav, NavItem, NavList,  } from '@patternfly/react-core';
 import { ApplyButton } from './ApplyButton';
